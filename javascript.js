e29f312b08f1ef8877ab24d5f7120b6c922cb9ff
aabbell <abelalemneh68@gmail.com>
2023-10-22 14:42:25 +0300

    edit javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-azsdxfcgvhbjnkm
+
+function getComputerchoice(){
+    let choicenum = Math.floor(Math.random()*3);
+    if (choicenum === 0){
+        return 'Rock';
+    }
+    else if (choicenum === 1){
+        return 'Paper';
+    }
+    else (choicenum === 2)
+       return 'Sciccors';
+}
+console.log(getComputerchoice());
