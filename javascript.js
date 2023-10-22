7999dca2e0828147736c4ca162794b907894a8bd
aabbell <abelalemneh68@gmail.com>
2023-10-22 16:15:36 +0300

    edit javascript.js

--- javascript.js
+++ javascript.js
@@ -13,28 +13,35 @@ function getComputerchoice(){
 
 
 function play_round(player_selection, computer_selection){
-    if (player_selection === 'rock' ||'Rock'||"ROCK" && computer_selection === 'paper'||'Paper'||'PAPER'){
+    if (player_selection = 'rock' && computer_selection == 'paper'){
         return "You Lose! Paper beats Rock";
     }
-    else if (player_selection === 'rock' ||'Rock'||"ROCK" && computer_selection === 'scissors'||'Scissors'||'SCISSORS'){
+    else if (player_selection = 'rock'  && computer_selection == 'scissors'){
         return "You Win! Rock beats Scissors";
     }
-    else if (player_selection === 'paper' ||'Paper'||"PAPER" && computer_selection === 'scissors'||'Scissors'||'SCISSORS'){
+    else if (player_selection = 'paper' && computer_selection == 'scissors'){
         return "You Lose! Scissors beats Rock"; 
     }
-    else if (player_selection === 'paper' ||'Paper'||"PAPER" && computer_selection === 'rock'||'Rock'||'ROCK'){
+    else if (player_selection = 'paper'  && computer_selection == 'rock'){
         return "You Win! Paper beats Rock";
     }
-    else if (player_selection === 'Scissors' ||'Scissors'||"SCISSORS" && computer_selection === 'rock'||'Rock'||'ROCK'){
+    else if (player_selection = 'Scissors' && computer_selection == 'rock'){
         return "You Lose! Rock beats Sciccors";
     }
-    else if (player_selection === 'Scissors' ||'Scissors'||"SCISSORS" && computer_selection === 'paper'||'Paper'||'PAPER'){
+    else if (player_selection = 'Scissors'  && computer_selection == 'paper'){
         return "You Win! Scissors beats Paper";
     }
     else {
         return "Draw";
     }
 }
-const player_selection = "rock";
-const computer_selection = getComputerchoice();
-console.log(play_round(player_selection,computer_selection));
+
+
+function game(){
+    for (let i = 1; i <= 5; i++){
+        const player_selection = prompt("write your choices");
+        const computer_selection = getComputerchoice();
+        console.log(play_round(player_selection,computer_selection));
+    }
+}
+console.log(game());
